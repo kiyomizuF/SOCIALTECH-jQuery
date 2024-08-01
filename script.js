e6f6eb7603f0ff0eb522b6fb585536d7176c9f3e
kiyomizuF <shimizu.itsuki@gmail.com>
2024-08-01 15:20:54 +0900

    送信ボタンクリック時と入力欄のblur時にinputCheckを呼び出すjsを追加

--- script.js
+++ script.js
@@ -27,4 +27,42 @@ $(function () {
     autoplaySpeed: 5000,
     arrows: false,
   });
+
+  // 送信ボタンクリック時の処理
+  $("#submit").on("click", function (event) {
+    // formタグによる送信を拒否
+    event.preventDefault();
+
+    // 入力チェックをした結果をresultに格納
+    let result = inputCheck();
+  });
+
+  $("#name").on("blur", function () {
+    inputCheck();
+  });
+
+  $("#furigana").on("blur", function () {
+    inputCheck();
+  });
+
+  $("#email").on("blur", function () {
+    inputCheck();
+  });
+
+  $("#tel").on("blur", function () {
+    inputCheck();
+  });
+
+  $("#message").on("blur", function () {
+    inputCheck();
+  });
+
+  $("#agree").on("click", function () {
+    inputCheck();
+  });
+
+  // お問い合わせフォームの入力チェック
+  function inputCheck() {
+    console.log("inputCheck関数の呼び出し");
+  }
 });
